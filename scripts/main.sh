6b699a05a981f1df7729954fe2525bd83eda7435
Sinkerine <git@15cm.net>
2020-10-16 22:36:50 -0700

    Merge entry scripts to main.sh. Add simple cache feature
    - The entry point is now main.sh; Options is parsed by `getopts`
    - Replace `wget` with `curl -f` to avoid saving files on failure. So that we can
    only downloads non-existing files.
    - Add a cache option to skip image metadata updating by full file paths in the
    cache file.

---
+++ scripts/main.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+deps=(
+  fd
+  parallel
+  jq
+  wget
+  realpath
+)
+
+for dep in $deps; do
+  if ! which $dep > /dev/null 2>&1; then
+    echo "Missing dependency: $dep"
+  fi
+done
+
+dir=$(dirname "$0")
+root_dir=$(dirname "${dir}")
+
+while getopts 'o:c:utdm' option; do
+  case "$option" in
+    o)
+      output_dir="$OPTARG"
+      ;;
+    c)
+      cache_file="$OPTARG"
+      ;;
+    u)
+      update_cache=y
+      ;;
+    t)
+      twitter_favorites_fetching=y
+      ;;
+    d)
+      download=y
+      ;;
+    m)
+      metadata_update=y
+      ;;
+    *)
+      echo "Usage : $(basename $0) <-o output_dir> [-c cache_file_for_metadata_update] [-u(update cache)] [-t(twitter favorites fetching)][-d(download)] [-m(metadata update)]"
+      echo "One of -t/-d/-m must be specified."
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$output_dir" ] || { [ -z "$twitter_favorites_fetching" ] && [ -z "$download" ] && [ -z "$metadata_update" ]; }; then
+  echo "Usage : $(basename $0) <-o output_dir> [-c cache_file_for_metadata_update] [-u(update cache)] [-t(twitter favorites fetching)][-d(download)] [-m(metadata update)]"
+  echo "One of -t/-d/-m must be specified."
+  exit 1
+fi
+
+if [ -n "$twitter_favorites_fetching" ]; then
+  bundle exec "${root_dir}/twitter-favorites-archive.rb" meta -o="${output_dir}"
+fi
+
+if [ -n "$download" ]; then
+  fd --no-ignore 'tweet\.json' "$output_dir" | parallel "${dir}/lib/download-media.sh -c \"$cache_file\" -f {}"
+fi
+
+if [ -n "$metadata_update" ]; then
+  fd --no-ignore '\.(png|jpg)$' "$output_dir" | parallel "${dir}/lib/update-media-meta.sh -c \"$cache_file\" -f {}"
+fi
+
+if [ -n "$update_cache" ]; then
+  fd -a --no-ignore '\.(png|jpg)$' "$output_dir" > "$cache_file"
+fi
