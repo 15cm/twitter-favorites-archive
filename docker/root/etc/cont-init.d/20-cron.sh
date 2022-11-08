ae839f29bd33c988cec137cb9b107f75f5d92e91
初神 <45256288+chu-shen@users.noreply.github.com>
2022-11-09 02:05:26 +0800

    fix

--- docker/root/etc/cont-init.d/20-cron.sh
+++ docker/root/etc/cont-init.d/20-cron.sh
@@ -2,7 +2,7 @@
 
 cat << EOF | crontab -
 ${CRON_SCHEDULE} /usr/bin/with-contenv bash -l -c \
-'echo "Archiving -- $(date)" > /var/log/twitter-favorites-archive/stdout.log \
+'echo "Archiving -- \$(date)" > /var/log/twitter-favorites-archive/stdout.log \
 && cd /app \
 && ./scripts/main.sh -tdmu -o ./output -c ./output/cache.txt >> /var/log/twitter-favorites-archive/stdout.log'
 EOF
