70d952448d8d56aa44e3a9a5bd6183b2efe05456
初神 <45256288+chu-shen@users.noreply.github.com>
2022-11-08 17:47:38 +0800

    add more metadata

--- scripts/lib/update-media-meta.sh
+++ scripts/lib/update-media-meta.sh
@@ -30,9 +30,13 @@ created_at=$(date -u -d"$(jq -r '.created_at' $meta_file)" +'%Y:%m:%d %H:%M:%S')
 # Extract the last https://t.co url
 full_text=$(jq -r '.full_text' "$meta_file" | perl -pe 'use utf8; s/(\shttps:\/\/t.co\/[a-zA-Z0-9]+)(?!.*\shttps:\/\/t.co\/[a-zA-Z0-9]+)//')
 url=$(jq -r '.full_text' "$meta_file" | perl -ne '/(https:\/\/t.co\/[a-zA-Z0-9]+)(?!.*https:\/\/t.co\/[a-zA-Z0-9]+)/ && print "$1\n";')
+user_name=$(jq -r '.user.name' "$meta_file")
+hashtags=$(jq -r '.entities.hashtags[].text' "$meta_file")
 
 if [ -z "$cache_file" ] || ! $(grep -Fqs "$file" "$cache_file"); then
-  exiftool -q -overwrite_original -DateTimeOriginal="${created_at}" -ImageDescription="${url}" -UserComment="${full_text}" "$file"
+
+  exiftool -q -overwrite_original -iptc:all -codedcharacterset=utf8 -charset iptc=UTF8 -DateTimeOriginal="${created_at}" -Subject="Twitter Favorites Archive Project" -artist="${user_name}" -ImageDescription="${full_text}" -comment="${url}" -keywords="${hashtags}" "$file"
+
   if [ $? -eq 0 ]; then
     echo "Updated meta data of $file."
   else
