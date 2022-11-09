77d522ba12f885eaeef4885c5faa69b34e13853b
初神 <45256288+chu-shen@users.noreply.github.com>
2022-11-09 23:07:42 +0800

    support download video

--- docker/root/etc/cont-init.d/20-cron.sh
+++ docker/root/etc/cont-init.d/20-cron.sh
@@ -4,5 +4,5 @@ cat << EOF | crontab -
 ${CRON_SCHEDULE} /usr/bin/with-contenv bash -l -c \
 'echo "Archiving -- \$(date)" > /var/log/twitter-favorites-archive/stdout.log \
 && cd /app \
-&& ./scripts/main.sh -tdmu -o ./output -c ./output/cache.txt >> /var/log/twitter-favorites-archive/stdout.log'
+&& ./scripts/main.sh -tdmu -o ./output -c ./output/cache.txt >> /var/log/twitter-favorites-archive/stdout.log 2>&1'
 EOF
